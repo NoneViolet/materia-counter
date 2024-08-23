4dab7fe4c310bf8cf5ec2ed0c99b93d88fd81df0
NoneViolet <snoachi@yahoo.co.jp>
2024-08-24 01:11:47 +0900

    累積確率を変更するとフリーズする不具合を修正

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,30 +1,29 @@
 import { useState } from "react"
 
 const Toolbar = ({ CProb, setCProb }) => {
-    const [CProbDisp, setCProbDisp] = useState(Math.round(CProb * 100));
+    const [CProbDisp, setCProbDisp] = useState(CProb * 100);
 
     const handleChangeCProb = (event) => {
         let newCProbDisp = event.target.value;
+        console.log(newCProbDisp)
         if (newCProbDisp > 99) { newCProbDisp = 99 }
         if (newCProbDisp < 1) { newCProbDisp = 1 }
         try {
             parseInt(newCProbDisp);
         }
         catch (Exception) {
-            return;
+            return null;
         }
 
-        setCProb(Math.round(newCProbDisp / 100));
+        setCProb(newCProbDisp / 100);
         setCProbDisp(newCProbDisp);
     };
 
     return (
         <div className="container">
             <h5>設定</h5>
-            <form className="CPForm">
-                <label>累積確率:</label><br />
-                <input type="number" style={{ width: 80 }} value={CProbDisp} onChange={handleChangeCProb}></input>
-            </form>
+            <label>累積確率:</label><br />
+            <input type="number" style={{ width: 80 }} value={CProbDisp} onChange={handleChangeCProb}></input>
         </div>
     )
 }
